3b55e8ab8de4558817d8a0977abe6fc13d60add2
Hayden Dingman <haydendingman@gmail.com>
2025-07-07 14:04:51 -0700

    Add main content block

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import './App.css'
 function App() {
 
   return (
-    <h1>I did it?</h1>
-  )
+    <main>
+        <img src="src/assets/react-logo.png" width="40px" />
+        
+        <h1>Fun facts about React</h1>
+        <ul>
+        <li>Was first released in 2013.</li>
+        <li>Was originally created by Jordan Walke.</li>
+        <li>Has well over 100K stars on Github.</li>
+        <li>Is maintained by Meta.</li>
+        <li>Powers thousands of enterprise apps, including mobile apps.</li>
+        </ul>
+    </main>
+    )
 }
 
 export default App
